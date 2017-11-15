53eed0ea79f32eda89e0a819314f25ad74e1f334
Alex White <VVu@geekfarm.org>
2017-11-14 21:31:52 -0800

    clean up whitespace

--- gofocus.go
+++ gofocus.go
@@ -25,24 +25,24 @@ type Task struct {
 var QUERY_BASE string = "SELECT persistentIdentifier, name, parent, dateToStart + 978307200, dateDue + 978307200, CAST(dateCompleted AS INT) + 978307200 FROM task "
 
 func createHandler(w http.ResponseWriter, r *http.Request) {
-    name		:= r.FormValue("name")
-    parent		:= r.FormValue("parent")
-    deferDate	:= r.FormValue("defer")
-    dueDate		:= r.FormValue("due")
+    name        := r.FormValue("name")
+    parent      := r.FormValue("parent")
+    deferDate   := r.FormValue("defer")
+    dueDate     := r.FormValue("due")
 
     if parent == "" {
         http.Error(w, "ERROR: parent not specified", http.StatusInternalServerError)
         return
     }
 
-	tasktxt := "--" + name + " ::" + parent + " #" + deferDate + " #" + dueDate
+    tasktxt := "--" + name + " ::" + parent + " #" + deferDate + " #" + dueDate
 
-	cmd := "/usr/bin/osascript"
-	args := []string{"-e", `tell application "OmniFocus"`,
-		"-e", `parse tasks into default document with transport text "` + tasktxt + `"`,
-		"-e", "end tell"}
+    cmd := "/usr/bin/osascript"
+    args := []string{"-e", `tell application "OmniFocus"`,
+        "-e", `parse tasks into default document with transport text "` + tasktxt + `"`,
+        "-e", "end tell"}
 
-	fmt.Println(cmd, args)
+    fmt.Println(cmd, args)
 
     output, err := exec.Command(cmd, args...).CombinedOutput()
     if err != nil {
@@ -81,7 +81,7 @@ func idHandler(w http.ResponseWriter, r *http.Request) {
 func queryHandler(w http.ResponseWriter, r *http.Request) {
     query := r.URL.Path[len("/query/"):]
 
-	db, dberr := openDB()
+    db, dberr := openDB()
     if dberr != nil {
         http.Error(w, dberr.Error(), http.StatusInternalServerError)
         return;
@@ -100,28 +100,28 @@ func queryHandler(w http.ResponseWriter, r *http.Request) {
     var dateDue int64
     var dateCompleted int64
 
-	tasks := make([]*Task, 0)
+    tasks := make([]*Task, 0)
 
-	idx := -1
+    idx := -1
     for rows.Next() {
-		rows.Scan(&persistentIdentifier, &name, &parent, &dateToStart, &dateDue, &dateCompleted)
+        rows.Scan(&persistentIdentifier, &name, &parent, &dateToStart, &dateDue, &dateCompleted)
 
-		dateToStartReal := time.Unix(dateToStart, 0)
-		dateDueReal := time.Unix(dateDue, 0)
+        dateToStartReal := time.Unix(dateToStart, 0)
+        dateDueReal := time.Unix(dateDue, 0)
 
-		var completeFlag bool
-		if dateCompleted == 0 {
-			completeFlag = false
-		} else {
-			completeFlag = true
-		}
+        var completeFlag bool
+        if dateCompleted == 0 {
+            completeFlag = false
+        } else {
+            completeFlag = true
+        }
 
-		t := &Task{Id: persistentIdentifier, Name: name, Parent: parent, Start: dateToStartReal, Due: dateDueReal, Completed: completeFlag}
+        t := &Task{Id: persistentIdentifier, Name: name, Parent: parent, Start: dateToStartReal, Due: dateDueReal, Completed: completeFlag}
 
-		idx++
-		//tasks[idx] = t
-		tasks = append(tasks, t)
-	}
+        idx++
+        //tasks[idx] = t
+        tasks = append(tasks, t)
+    }
 
     rows.Close()
     db.Close()
@@ -137,15 +137,15 @@ func queryHandler(w http.ResponseWriter, r *http.Request) {
 func doneHandler(w http.ResponseWriter, r *http.Request) {
     id := r.URL.Path[len("/done/"):]
 
-	cmd := "/usr/bin/osascript"
-	args := []string{"-e", `tell application "OmniFocus"`,
-		"-e", "tell default document",
-		"-e", `set selectedTask to first flattened task whose (id = "jnixie_Wx0n")`,
-		"-e", "set completed of selectedTask to true",
-		"-e", "end tell",
-		"-e", "end tell"}
+    cmd := "/usr/bin/osascript"
+    args := []string{"-e", `tell application "OmniFocus"`,
+        "-e", "tell default document",
+        "-e", `set selectedTask to first flattened task whose (id = "jnixie_Wx0n")`,
+        "-e", "set completed of selectedTask to true",
+        "-e", "end tell",
+        "-e", "end tell"}
 
-	fmt.Println(cmd, args)
+    fmt.Println(cmd, args)
 
     output, err := exec.Command(cmd, args...).CombinedOutput()
     if err != nil {
@@ -160,15 +160,15 @@ func doneHandler(w http.ResponseWriter, r *http.Request) {
 func undoneHandler(w http.ResponseWriter, r *http.Request) {
     id := r.URL.Path[len("/undone/"):]
 
-	cmd := "/usr/bin/osascript"
-	args := []string{"-e", `tell application "OmniFocus"`,
-		"-e", "tell default document",
-		"-e", `set selectedTask to first flattened task whose (id = "jnixie_Wx0n")`,
-		"-e", "set completed of selectedTask to false",
-		"-e", "end tell",
-		"-e", "end tell"}
+    cmd := "/usr/bin/osascript"
+    args := []string{"-e", `tell application "OmniFocus"`,
+        "-e", "tell default document",
+        "-e", `set selectedTask to first flattened task whose (id = "jnixie_Wx0n")`,
+        "-e", "set completed of selectedTask to false",
+        "-e", "end tell",
+        "-e", "end tell"}
 
-	fmt.Println(cmd, args)
+    fmt.Println(cmd, args)
 
     output, err := exec.Command(cmd, args...).CombinedOutput()
     if err != nil {
@@ -188,11 +188,11 @@ func openDB() (*sql.DB, error) {
         dbfile = os.Getenv("DBFILE")
     }
     db, _ := sql.Open("sqlite3", "file:" + dbfile + "?mode=ro")
-	return db, nil
+    return db, nil
 }
 
 func loadId(id string) (*Task, error) {
-	db, dberr := openDB()
+    db, dberr := openDB()
     if dberr != nil {
         return nil, dberr
     }
